f91ef12c36ff9abc5bcb5dd4ecd01a0556b918b3
saeyeong <syeongppp@gmail.com>
2022-08-19 14:53:24 +0900

    BaseActivity 추가, minSdk 24 변경

---
+++ app/src/main/java/com/example/kurlyview/BaseActivity.kt
@@ -0,0 +1,19 @@
+package com.example.kurlyview
+
+import android.os.Bundle
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
+
+    protected val viewBinding: VB by lazy { inflateViewBinding() }
+
+    abstract fun inflateViewBinding(): VB
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(viewBinding.root)
+    }
+}
