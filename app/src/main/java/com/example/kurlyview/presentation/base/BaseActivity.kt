7b4cc5beecc8814ad50ff14732ea0411228355de
saeyeong <syeongppp@gmail.com>
2022-08-23 09:20:42 +0900

    BaseActivity 수정, BaseFragment 추가

--- app/src/main/java/com/example/kurlyview/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/example/kurlyview/presentation/base/BaseActivity.kt
@@ -3,11 +3,15 @@ package com.example.kurlyview.presentation.base
 import android.os.Bundle
 import androidx.annotation.CallSuper
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.viewbinding.ViewBinding
+import java.lang.reflect.ParameterizedType
 
-abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
+abstract class BaseActivity<VB: ViewBinding, VM: ViewModel>: AppCompatActivity() {
 
     protected val viewBinding: VB by lazy { inflateViewBinding() }
+    protected lateinit var viewModel: VM
 
     abstract fun inflateViewBinding(): VB
 
@@ -15,5 +19,16 @@ abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+
+        val parameterizedType = javaClass.genericSuperclass as? ParameterizedType
+
+        val vmClass = parameterizedType?.actualTypeArguments?.getOrNull(1) as? Class<VM>
+
+        vmClass?.let {
+            viewModel = ViewModelProvider(this).get(it)
+        } ?: run {
+            finish()
+            return
+        }
     }
 }
---
+++ app/src/main/java/com/example/kurlyview/presentation/base/BaseFragment.kt
@@ -0,0 +1,23 @@
+package com.example.kurlyview.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VB: ViewBinding>: Fragment() {
+    lateinit var viewBinding: VB
+
+    abstract fun inflateViewBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = inflateViewBinding(inflater, container)
+        return viewBinding.root
+    }
+}
